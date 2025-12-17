f96f5b9f53be388142413c0a2970711abb5eac9d
SridhiviyaS <90917907+SridhiviyaS@users.noreply.github.com>
2025-12-17 09:40:29 +0530

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,10 +5,16 @@ set -e
 docker build -t gist-api -f docker/Dockerfile .
 
 # Get ECR repo URL from Terraform output
+cd terraform
 ECR_REPO=$(terraform output -raw ecr_repository_url)
+cd ..
+
+echo "DEBUG: ECR_REPO = $ECR_REPO"
 
 # Extract registry URL (remove repository name for docker login)
 ECR_REGISTRY=$(echo $ECR_REPO | cut -d'/' -f1)
+
+echo "DEBUG: ECR_REGISTRY = $ECR_REGISTRY"
 
 # Login to ECR
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REGISTRY
